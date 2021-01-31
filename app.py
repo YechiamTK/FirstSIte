d791acd3082e5c6ef9d29d5825e4b80e154a778e
Yechiam Weiss <yyyopla3211@gmail.com>
2021-01-31 16:40:09 +0200

    stupid.

--- app.py
+++ app.py
@@ -10,12 +10,12 @@ app.config.from_mapping(
     DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
 )
 
-if test_config is None:
+#if test_config is None:
     # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
+#    app.config.from_pyfile('config.py', silent=True)
+#else:
     # load the test config if passed in
-    app.config.from_mapping(test_config)
+#    app.config.from_mapping(test_config)
 
 # ensure the instance folder exists
 try:
