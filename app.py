38f12704deb4cc2c2cbd04945cc7180f0c7a7efa
Yechiam Weiss <yyyopla3211@gmail.com>
2021-01-31 16:37:32 +0200

    try3

--- app.py
+++ app.py
@@ -3,44 +3,43 @@ from flask import Flask
 
 app = Flask(__name__, instance_relative_config=True)
 
-def config_app(flapp, test_config=None):
+#def config_app(app, test_config=None):
     #create&config
-    flapp.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(flapp.instance_path, 'db.sqlite'),
-    )
+app.config.from_mapping(
+    SECRET_KEY='dev',
+    DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        flapp.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        flapp.config.from_mapping(test_config)
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(flapp.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    #@flapp.route('/hello')
-    #def hello():
-    #    return 'Hello, World!'
+#@app.route('/hello')
+#def hello():
+#    return 'Hello, World!'
 
-    import db
-    db.init_app(flapp)
+import db
+db.init_app(app)
 
-    #import posts
-    #flapp.register_blueprint(posts.bp)
-    #flapp.add_url_rule('/', endpoint='index')
-    #flapp.add_url_rule('/', endpoint='postTweet')
+import posts
+app.register_blueprint(posts.bp)
+app.add_url_rule('/', endpoint='index')
+#app.add_url_rule('/', endpoint='postTweet')
 
-    return flapp
+#    return app
 
-@app.route('/')
-def index():
-    return "<h1>Welcome to our server !!</h1>"
+#@app.route('/')
+#def index():
+#    return "<h1>Welcome to our server !!</h1>"
 
 if __name__ == "__main__":
-    app = config_app(app)
     app.run()
