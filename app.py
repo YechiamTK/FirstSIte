1c8d03e2824e33840842f592bc2624c73a87c7d2
Yechiam Weiss <yyyopla3211@gmail.com>
2021-01-31 14:21:53 +0200

    Hopefuly last?

--- app.py
+++ app.py
@@ -1,40 +1,42 @@
 import os
 from flask import Flask
 
-def create_app(test_config=None):
+app = Flask(__name__, instance_relative_config=True)
+
+def config_app(flapp, test_config=None):
     #create&config
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
+    flapp.config.from_mapping(
         SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'db.sqlite'),
+        DATABASE=os.path.join(flapp.instance_path, 'db.sqlite'),
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        flapp.config.from_pyfile('config.py', silent=True)
     else:
         # load the test config if passed in
-        app.config.from_mapping(test_config)
+        flapp.config.from_mapping(test_config)
 
     # ensure the instance folder exists
     try:
-        os.makedirs(app.instance_path)
+        os.makedirs(flapp.instance_path)
     except OSError:
         pass
 
-    #@app.route('/hello')
+    #@flapp.route('/hello')
     #def hello():
     #    return 'Hello, World!'
 
     import db
-    db.init_app(app)
+    db.init_app(flapp)
 
     import posts
-    app.register_blueprint(posts.bp)
-    app.add_url_rule('/', endpoint='index')
-    #app.add_url_rule('/', endpoint='postTweet')
+    flapp.register_blueprint(posts.bp)
+    flapp.add_url_rule('/', endpoint='index')
+    #flapp.add_url_rule('/', endpoint='postTweet')
 
-    return app
+    return flapp
 
 if __name__ == "__main__":
-    application = create_app()
+    app = config_app()
+    app.run()
